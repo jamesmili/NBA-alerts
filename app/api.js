099807028e4925ea098ed32e7993b691f6533821
James Militante <militajo@mcmaster.ca>
2017-07-14 21:44:30 -0400

    set up twitter api

---
+++ app/api.js
@@ -0,0 +1,15 @@
+var Twitter = require('twitter');
+
+var client = new Twitter({
+  consumer_key: 'Ok79uJKb56v8r128AssSFnzqF',
+  consumer_secret: 'rRC7cGcfFFW3HviwzRcNPeqf29qXiPix4xxtntu1dgZNwYgu0S',
+  access_token_key: '4061490593-R6yU3JGhWsZBdXARXQ1Zvy6DdyWvBgUhpzgGfA2',
+  access_token_secret: '6h63nrICrxo5BHnIeNvyVsEiLxeRcHLnYCgrfFDVD80fA'
+});
+
+var params = {screen_name: 'wojespn'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  }
+});
