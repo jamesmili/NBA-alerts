b4047601d1e15b81180057a50667cb5d97c475e7
James Militante <militajo@mcmaster.ca>
2017-07-14 21:57:08 -0400

    working tweets api

--- app/api.js
+++ app/api.js
@@ -10,6 +10,8 @@ var client = new Twitter({
 var params = {screen_name: 'wojespn'};
 client.get('statuses/user_timeline', params, function(error, tweets, response) {
   if (!error) {
-    console.log(tweets);
+    for(var i = 0; i < tweets.length; i++){
+      console.log(tweets[i].text);
+    }
   }
 });
