6350b5bb93cbedd9fb2e43245fe8cb4769df75e5
James Militante <militajo@mcmaster.ca>
2017-07-21 11:40:06 -0400

    added alerts with reddit api

---
+++ background.js
@@ -0,0 +1,41 @@
+getData()
+.then(info => {
+  let options = {
+    type: "basic",
+    title: "Wojbomb",
+    message: info.data,
+    iconUrl: "woj-bomb.png"
+  }
+  chrome.notifications.create(options);
+
+  return info;
+})
+.then(info =>{
+
+})
+
+setInterval(function(){
+
+
+ }, 2000)
+
+function getData(){
+  return new Promise((resolve,reject) => {
+    $.getJSON(
+      "https://www.reddit.com/r/nba/search.json?q=%5Bwojnarowski%5D&sort=new&restrict_sr=on&t=all",
+      function foo(data)
+      {
+        $.each(
+          data.data.children.slice(0, 10),
+          function (i, post) {
+            if (post.data.title.includes("[Wojnarowski]")){
+              let data = post.data.title;
+              let link = post.data.url;
+              resolve({data, link});
+            }
+          }
+        )
+      }
+    )
+  })
+}
