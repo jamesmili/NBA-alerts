71f5fd58351cd3cce486ec0fd48795c26ac5b528
James Militante <militajo@mcmaster.ca>
2017-07-21 18:06:16 -0400

    added interval and chrome-extension icon

--- background.js
+++ background.js
@@ -1,36 +1,34 @@
-getData()
-.then(info => {
-  let options = {
-    type: "basic",
-    title: "Wojbomb",
-    message: info.data,
-    iconUrl: "woj-bomb.png"
-  }
-  chrome.notifications.create(options);
-  chrome.notifications.onClicked.addListener(function(){
-    chrome.tabs.create({url: info.link})
-  })
-  return info;
-})
-.then(info =>{
-
-})
-
 setInterval(function(){
+  getData()
+  .then(info => {
+    let options = {
+      type: "basic",
+      title: "Wojbomb",
+      message: info.data,
+      iconUrl: "woj-bomb.png"
+    }
+    chrome.notifications.create(options);
+    chrome.notifications.onClicked.addListener(function(){
+      chrome.tabs.create({url: info.link})
+    })
+    return info;
+  })
+  .then(info =>{
+    store(info.link)
+  })
+}, 60000)
 
-
- }, 2000)
 
 function getData(){
   return new Promise((resolve,reject) => {
     $.getJSON(
-      "https://www.reddit.com/r/nba/search.json?q=%5Bwojnarowski%5D&sort=new&restrict_sr=on&t=all",
+      "http://www.reddit.com/r/nba/new.json?sort=new&limit=10&t=hour",
       function getData(data)
       {
         $.each(
           data.data.children.slice(0, 10),
           function (i, post) {
-            if (post.data.title.includes("[Wojnarowski]")){
+            if (post.data.title[0] === "["){
               let data = post.data.title;
               let link = post.data.url;
               resolve({data, link});
@@ -39,5 +37,15 @@ function getData(){
         )
       }
     )
+  })
+}
+
+function store(url){
+  return new Promise((resolve,reject) => {
+    chrome.storage.local.get(function(data){
+      var links = data.links || [];
+      links.push(url);
+      chrome.storage.local.set({links})
+    })
   })
 }
