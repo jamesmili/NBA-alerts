604a2688f2f5193ca33daba40fd157a6306eb51d
James Militante <james.militante@gmail.com>
2020-12-11 17:03:17 -0500

    ignores duplicates

--- src/utils/api.js
+++ src/utils/api.js
@@ -6,12 +6,16 @@ import axios from "axios";
 */
 
 export var getTweets = () => {
+  var urls = new Set()
   var url = "https://www.reddit.com/r/nba/search.json?q=url%3Atwitter&restrict_sr=on&sort=new&t=week";
   return axios.get(url).then(res =>{
     var posts = []
+    var tweetUrl
     res.data.data.children.map(function(obj) {
-      if (obj.data.url.includes("twitter")){
+      tweetUrl = obj.data.url.toLowerCase()
+      if (obj.data.url.includes("twitter") && !urls.has(tweetUrl)){
         posts.push(obj.data)
+        urls.add(tweetUrl)
       }
     });
     return posts;
